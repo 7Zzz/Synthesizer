9d85cdccabeb784b9730007a75220eb626a90987
Ivan Tolmachev <ivatolm@gmail.com>
2018-11-21 20:47:02 +0300

    Modified MCP_button(returning array now) and remaked memorize_func

--- My_HAL/Src/MCP_button.c
+++ My_HAL/Src/MCP_button.c
@@ -17,24 +17,26 @@ void MCP_Button_Init(BTN_TypeDef* b, u8 num, IO_PortTD port, IO_PinTD pin) {
 
 void MCP_Buttons_InitAll(void) {
 	for (u8 i = 0; i < NUM_OF_BTNs; i++) {
-		MCP_Button_Init(&buttons[i], i, i / 8, i % 8);  // auto-assign num, port and pin
+		MCP_Button_Init(&buttons[i], i, i / 8, i % 8); // auto-assign num, port and pin
 	}
 	buttonMsg[0] = 'b';
 	buttonMsg[1] = 0;
 	buttonMsg[2] = UNPRESSED;
 }
 
-void BTN_Read_All(u8 mcp_addr) {
+int* BTN_Read_All(u8 mcp_addr) {
+	int btn_pressed[NUM_OF_BTNs];
+
 	u8 mcpA = MCP23017_ReadPort(mcp_addr, PORTA);
 	u8 mcpB = MCP23017_ReadPort(mcp_addr, PORTB);
 	u8 read = 0;
 	for (u8 i = 0; i < NUM_OF_BTNs; i++) {
 		switch (i / 8) {
 		case 0:
-			read = (mcpA >> buttons[i].pin) & 0x01;//вроде дает двоичный код c 1-8 кнопку который надо передать расширителю портов
+			read = (mcpA >> buttons[i].pin) & 0x01; //вроде дает двоичный код c 1-8 кнопку который надо передать расширителю портов
 			break;
 		case 1:
-			read = (mcpB >> buttons[i].pin) & 0x01;//вроде дает двоичный код c 8-16 кнопку который надо передать расширителю портов
+			read = (mcpB >> buttons[i].pin) & 0x01; //вроде дает двоичный код c 8-16 кнопку который надо передать расширителю портов
 			break;
 		}
 		if (millis - buttons[i].prevTime > INTERVAL) {
@@ -42,21 +44,26 @@ void BTN_Read_All(u8 mcp_addr) {
 				buttons[i].state = PRESSED;
 				buttons[i].prevTime = millis;
 				buttonMsg[1] = i;              // fill msg - button number
-				buttonMsg[2] = PRESSED;        // button state
-			//for(u8 k = 0; k<3; k++)
-			//Reg_Send(&buttonMsg[k]);         // action  ?
-		} else if (!read && buttons[i].state == PRESSED) {
-			buttons[i].state = UNPRESSED;
-			buttons[i].prevTime = millis;
-			buttonMsg[1] = i;                  // fill msg - button number
-			buttonMsg[2] = UNPRESSED;          // button state
-			//for (u8 k = 0; k < 3; k++)
+				buttonMsg[2] = PRESSED; 	   // button state
+				//for(u8 k = 0; k<3; k++)
+				//Reg_Send(&buttonMsg[k]);     // action  ?
+				if (btn_pressed[i] == 0)
+					btn_pressed[i] = 1;
+				else
+					btn_pressed[i] = 0;
+
+			} else if (!read && buttons[i].state == PRESSED) {
+				buttons[i].state = UNPRESSED;
+				buttons[i].prevTime = millis;
+				buttonMsg[1] = i;                  // fill msg - button number
+				buttonMsg[2] = UNPRESSED;          // button state
+				//for (u8 k = 0; k < 3; k++)
 				//Reg_Send(&buttonMsg[k]);     // action   ?
+			}
 		}
-	}
-	if (buttons[i].prevTime > millis)
-		buttons[i].prevTime = 0;
-}
+		if (buttons[i].prevTime > millis)
+			buttons[i].prevTime = 0;
+	} return btn_pressed;
 }
 
 // TODO:
@@ -64,6 +71,4 @@ void BTN_Read_All(u8 mcp_addr) {
 //
 //	return 0;
 //}
-
-
 
--- My_HAL/Src/sequencer.c
+++ My_HAL/Src/sequencer.c
@@ -12,28 +12,31 @@ int leds_on[NUM_OF_BTNs];
 void memorize_buttons(void) {  // metronome
 	//function to memorize the pressed buttons and highlight them
 	//u8 r, g, b = LEDbuffer[0]; // сделать возврат цвета после выполнения функции
-	setWHOLEcolor(0, 0, 0);
+	setWHOLEcolor(COLOR_OFF); // обнулить все цвета на клавиатуре, на цвет не выбрано
 	DIG_LED_update();
-	u8 turn_state = BTN_Read_All(MCP23017_ADDR(15));
+	buttons = BTN_Read_All(MCP23017_ADDR(3));
+	u8 turn_state = buttons[15].state;
 	while (turn_state) {
+		buttons = BTN_Read_All(MCP23017_ADDR(3));
+		turn_state = buttons[15].state;
 		if (buttons[15].state) {
 			turn_state = !turn_state;
 			break;
 		} else {
 			for (int i = 0; i <= NUM_OF_BTNs; i++) {
 				if (buttons[i].state == 1) {
-					if (leds_on[i] == 1) {
-						leds_on[i] = 0;
+					if (leds_on[i] == 0) {
+						leds_on[i] = 1;
 						setLEDcolor(i, COLOR_ON);
 					} else {
-						leds_on[i] = 1;
+						leds_on[i] = 0;
 						setLEDcolor(i, COLOR_OFF);
 					}
 					DIG_LED_update();
 				}
 			}
 		}
-//		setWHOLEcolor(r, g, b);
+//		setWHOLEcolor(r, g, b); // вернуть значения до начала функции
 		DIG_LED_update();
 	}
 }
