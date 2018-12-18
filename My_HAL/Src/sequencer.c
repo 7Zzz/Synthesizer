e2b6218e5ae05791d5ad97d45df90e1824043cae
7Zzz <43302297+7Zzz@users.noreply.github.com>
2018-12-18 22:41:30 +0300

    small сorrection

--- My_HAL/Src/sequencer.c
+++ My_HAL/Src/sequencer.c
@@ -5,9 +5,10 @@ void save(u8 index, u8 value)
 	u8 pressing_index = btns[index].counter_pressing;
 	if (btns[index].state == UNPRESSED && value == PRESSED)
 	{
-		btns[index].state = 1; // saving button state into structure btns
-		time_periods[index][pressing_index] = millis; //save period into array
+		btns[index].state = 1;                                                   //saving button state into structure btns
+		time_periods[index][pressing_index] = millis;                            //save period into array
 		btns[index].tap_millis = millis;
+		the_begining_millis = btns[0].tap_millis + first_pause;
 	}
 	else if (btns[index].state == PRESSED && value == PRESSED)
 		return;
@@ -16,34 +17,51 @@ void save(u8 index, u8 value)
 	{
 		time_periods[index][pressing_index] = abs(
 				millis - time_periods[index][pressing_index]);
-		btns[index].counter_pressing += 1; // adding one to pressing counter
+		btns[index].counter_pressing += 1;                                       //adding one to pressing counter
 	}
 	else
-		btns[index].state = 0; // saving button state into structure btns
+		btns[index].state = 0;                                                   //saving button state into structure btns
 
 }
 
 void replay()
 {
-
+	for(int i = 0; i < NUM_OF_BTNs; i++)
+	{
+		if(millis - the_begining_millis >= btns[i].tap_millis)
+		{
+			btns[i].state = 1;
+		}
+		else
+		{
+			btns[i].state = 0;
+		}
+	}
 }
 
 void sequencer_func(u8* btn_states)
 {
 
-	if (btns[RECORD_BTN].state == PRESSED) // RECORD_BTN it's record activator this is not necessarily a button
+	if (btns[RECORD_BTN].state == PRESSED)                     //RECORD_BTN it's record activator this is not necessarily a button
+	{
 		state = !state;
-
+	}
 	if (state == RECORD_ON)
 	{
-		setWHOLEcolor(RECORD_OFF_COLOR); // turning led off
+		setWHOLEcolor(RECORD_OFF_COLOR);                       //turning led off
 		DIG_LED_update();
 
 		for (int i = 0; i < NUM_OF_BTNs; i++)
-			save(i, btn_states[i]); // updating state and starting timer(so and stopping it)
+		{
+			save(i, btn_states[i]);                            //updating state and starting timer(so and stopping it)
+		}
+
 		light();
+
 		for (int i = 0; i < NUM_OF_BTNs; i++)
+		{
 			btns[i].state = btn_states[i];
+		}
 	}
 }
 
