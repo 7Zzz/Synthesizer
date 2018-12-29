2a160f3591709833a975de569b3583ee165436a1
ivatolm <ivatolm@gmail.com>
2018-12-29 15:47:02 +0300

    Sequencer on the final stage

--- My_HAL/Inc/sequencer.h
+++ My_HAL/Inc/sequencer.h
@@ -10,36 +10,39 @@
 
 #include "MCP_button.h"
 #include "dig_LED.h"
-#include <stdlib.h>
 
-#define SELECTED 0,255,0
+#define RECORD_SELECTED 200,150,0
+#define REPLAY_SELECTED 0,150,200
 #define NOT_SELECTED 0,0,0
-#define REPLAY_ON_COLOR 0,0,100
-#define REPLAY_OFF_COLOR 0,0,0
-#define RECORD_ON_COLOR 100,0,0
-#define RECORD_OFF_COLOR 0,0,0
+
+#define REPLAY_ON_COLOR 100,100,100
+#define RECORD_ON_COLOR 50,50,50
+
 #define ON  1
 #define OFF 0
 #define RECORD_BTN 0
 #define REPLAY_BTN 1
 
+#define LED_INTERVAL 400
+
 u8 record_state;
 u8 replay_state;
-unsigned long replay_start;
+u8 selected_start;
+u8 current_btn;
+unsigned long previous_light;
+
 typedef struct
 {
 	u8 state;
 	u8 previous_pressing;
-	unsigned long selected_start;
+	u8 selected;
+	u8 replaying;
 } btn;
 
 btn btns[NUM_OF_BTNs];
 
 extern volatile uint32_t millis;
 
-void replay(void);
-void save(u8 index, u8 value);
 void sequencer_func(u8* i);
-void light(void);
 
 #endif /* SEQUENCER_H_ */
--- My_HAL/Src/sequencer.c
+++ My_HAL/Src/sequencer.c
@@ -1,38 +1,50 @@
 #include "sequencer.h"
 
 void sequencer_func(u8* btn_states) {
-	if (btn_states[RECORD_BTN] == PRESSED) //RECORD_BTN it's record activator this is not necessarily a button
+	if (btn_states[RECORD_BTN] == PRESSED) { //RECORD_BTN it's record activator this is not necessarily a button
 		record_state = !record_state;
-	if (btn_states[REPLAY_BTN] == PRESSED){ //RECORD_BTN it's record activator this is not necessarily a button
+		setWHOLEcolor(NOT_SELECTED);
+	}
+	if (btn_states[REPLAY_BTN] == PRESSED) { //RECORD_BTN it's record activator this is not necessarily a button
 		replay_state = !replay_state;
-		replay_start = millis;
+		previous_light = millis;
+		current_btn = 0;
+		setWHOLEcolor(NOT_SELECTED);
 	}
 
 	if (record_state == ON) {
-
+		setWHOLEcolor(RECORD_ON_COLOR);
 		for (int i = 0; i < NUM_OF_BTNs; i++)
 			if (btn_states[i] == PRESSED
 					&& millis - btns[i].previous_pressing > INTERVAL) {
 				btns[i].state = UNPRESSED;
 				btns[i].previous_pressing = millis;
+				btns[i].selected = !btns[i].selected;
 			}
 
 		for (int i = 0; i < NUM_OF_BTNs; i++)
-			if (btns[i].state == 1) {
-				setLEDcolor(i, SELECTED);
-				btns[i].selected_start = millis;
+			if (btns[i].selected == 1) {
+				setLEDcolor(i, RECORD_SELECTED);
 			} else {
 				setLEDcolor(i, NOT_SELECTED);
 			}
 	}
-	if(replay_state == ON)
-	{
-		for (int i = 0; i < NUM_OF_BTNs; i++) {
-			if (millis - replay_start > btns[i].selected_start) {
-				setLEDcolor(i, SELECTED);
+	if (replay_state == ON) {
+		setWHOLEcolor(NOT_SELECTED);
+		if (btns[current_btn].selected == 1) {
+			if (millis - previous_light > LED_INTERVAL) {
+				previous_light = millis;
+				setLEDcolor(current_btn, REPLAY_SELECTED);
+			}
+		}
+		for (int i = (current_btn+1); i < (NUM_OF_BTNs+(current_btn)); i++) {
+			if (i > NUM_OF_BTNs)
+				i = 0;
+			if (btns[i].selected == 1 && i != current_btn) {
+				current_btn = i;
+				break;
 			}
 		}
 	}
 	DIG_LED_update();
 }
-
